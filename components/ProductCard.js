330e79a95e9b13ac6ffe29194f696aa00cc4d865
ASSOGBA <andreassogba9@gmail.com>
2025-10-16 23:33:33 +0100

    Create ProductCard.js

---
+++ components/ProductCard.js
@@ -0,0 +1,11 @@
+// /components/ProductCard.js
+export default function ProductCard({ produit }) {
+  return (
+    <div className="product-card card">
+      <img src={produit.image_url || 'https://via.placeholder.com/400x300?text=No+Image'} alt={produit.nom} />
+      <h3>{produit.nom}</h3>
+      <p className="price">{produit.prix} FCFA</p>
+      <p style={{ color:'#666', fontSize:13 }}>{produit.description}</p>
+    </div>
+  )
+  }
